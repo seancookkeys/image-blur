ea91e65670fd4fb032fe9d6d5f04bde37242b941
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-07-21 00:14:31 +0000

    initial commit

---
+++ lib/image.rb
@@ -0,0 +1,13 @@
+class Image
+    attr_accessor :data
+  
+    def initialize(data)
+      @data = data
+    end
+  
+    def blur
+      # bad code
+      return Image.new(@data)
+    end
+  end
+  
